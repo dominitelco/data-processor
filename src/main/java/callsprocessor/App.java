c4557bd00c521736ef352bb5cd60c461c0d64461
Lorenzo Solano Martinez <lsolano@novosit.com>
2020-08-12 16:10:50 -0400

    Initial commit.

---
+++ src/main/java/callsprocessor/App.java
@@ -0,0 +1,26 @@
+package dataprocessor;
+
+/**
+ * Calls processor...
+ */
+public class App 
+{
+    public static void main(final String[] args)
+    {
+        if (args.length == 0) {
+            System.err.println("No arguments received, at least one is require with the full path of the file to analyze.");
+            return;
+        }
+
+        readFileAsList(args[0]);
+    }
+
+    private static void readFileAsList(final String fileFullPath) {
+        CallsFileReader reader = new FileSystemCallsFileReader(fileFullPath);
+        CallsFileDecorator decorator = new CallsFileDecorator(reader);
+
+        for (String line : decorator.decorateLines()) {
+            System.out.println(line);
+        }
+    }
+}
---
+++ src/main/java/callsprocessor/CallsFileReader.java
@@ -0,0 +1,7 @@
+package dataprocessor;
+
+import java.util.List;
+
+public interface CallsFileReader {
+    public List<String> readAllLines();
+}
